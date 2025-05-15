d72ce32c740e6bbb967d24d1b3bd328d1a83fdd1
戴航 <dhdwyyx@163.com>
2025-05-15 20:05:35 +0800

    第一次

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void sort(int arr[],int size)
+{
+
+}
+int main()
+{
+  int arr[]={5,8,96,5,1,32,10,25};
+  int size=sizeof(arr)/sizeof(arr[0]);
+  sort(arr,size);
+  return 0;
+}
